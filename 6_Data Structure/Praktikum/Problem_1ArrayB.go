3601b5cf7fb0f7d46105a8ab33bfab9479dbb443
LUQMAN WIRANTO <81842850+luqmanwiranto@users.noreply.github.com>
2022-05-17 17:10:39 +0800

    Create Problem_1ArrayB.go

---
+++ 6_Data Structure/Praktikum/Problem_1ArrayB.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	///arrayA
+	arraySatu := []string{"lee", "jin"}
+	arrayDua := []string{"kazuya", "panda"}
+	fmt.Println(arrayMerge(arraySatu, arrayDua))
+}
+func arrayMerge(arraySatu, arrayDua []string) (hasilGabung []string) {
+	hmap := make(map[string]bool)
+	for _, ValueSatu := range arraySatu {
+		hmap[ValueSatu] = true
+		hasilGabung = append(hasilGabung, ValueSatu)
+	}
+	for _, ValueDua := range arrayDua {
+		_, isFound := hmap[ValueDua]
+		if isFound == false {
+			hmap[ValueDua] = true
+			hasilGabung = append(hasilGabung, ValueDua)
+		}
+	}
+	return hasilGabung
+}
